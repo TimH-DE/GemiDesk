a644ae80ff19bc764b52ccc98bf5f5fc373ea354
Tim H. <79646946+TimH-DE@users.noreply.github.com>
2026-05-13 11:33:46 +0200

    Set application user model ID for GemiDesk

--- src/main/main.ts
+++ src/main/main.ts
@@ -27,6 +27,7 @@ app.commandLine.appendSwitch('user-agent', userAgent);
 app.setName('GemiDesk');
 app.userAgentFallback = userAgent;
 app.commandLine.appendSwitch('disable-infobars');
+app.setAppUserModelId('com.timh.gemidesk');
 
 process.env.DIST = path.join(__dirname, '../../dist');
 process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
